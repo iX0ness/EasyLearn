1fdb628b4637d7c3c61ef599e45656d5fb446bef
iX0ness <mykhaylo.levchuk@ukr.net>
2019-08-18 19:09:36 +0200

    work commit

--- EasyLearn/View/WordsLibrary/UIKit/LibraryViewController.swift
+++ EasyLearn/View/WordsLibrary/UIKit/LibraryViewController.swift
@@ -1,23 +1,18 @@
 //
-//  LibraryTableViewViewController.swift
+//  LibraryViewController.swift
 //  EasyLearn
 //
-//  Created by Levchuk Misha on 15/08/2019.
+//  Created by Levchuk Misha on 18/08/2019.
 //  Copyright © 2019 Levchuk Misha. All rights reserved.
 //
 
 import UIKit
 
-class LibraryTableViewViewController: UIViewController {
+class LibraryViewController: UIViewController {
     @IBOutlet weak var libraryTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-
-
-    
-
 }
